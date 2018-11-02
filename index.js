7ed01a62f4eaf778fbc02e595f09066220cc1953
njnisarg <NJnisarg@gmail.com>
2018-11-02 19:51:50 +0530

    Added proper comments in the files

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const _ = require('lodash');
 // Global Constants
 const baseRegistryUrl = 'https://registry.npmjs.org';
 
+
+// Function to resolve a version number using the semver versioning standards
 const resolveVersion = async ({pkgName,version}) => {
 
     // The function returns the maxSatisfying version that can be downloaded, if the version string cannot match the criteria
@@ -58,6 +60,8 @@ const resolveVersion = async ({pkgName,version}) => {
     }
 };
 
+
+// Function to the list of immediate dependencies( level - 1) for a given package
 const getDependencyList = async ({pkgName, version}) => {
     let ver = await resolveVersion({pkgName,version});
     try {
@@ -90,13 +94,21 @@ const getDependencyList = async ({pkgName, version}) => {
 
 };
 
+// The function to resolve the complete dependency graph for a single package
+// Returns the flat dependency list
 const resolveDependencies = async({pkgName,version}) => {
     // Takes the name of the source Package, version and the List of dependencies
     // Generates a final list of all the flat resolved dependencies
-    // dependencyList is an array of objects of the format {pkgName,version}
+
+    // Uses Breath First search to probe all the possible dependencies to a package given.
+    // Uses the BFS algorithm in an iterative fashion
+
+    // dList is the final flat dependency List that is formed. blackList is the package that is already explored
+    // grayList is the package in the Queue that is being processed. The Queue is a FIFO Queue.
+
+    let q = new Q();
 
     let dList = [];
-    let q = new Q();
     let blackList = [];
     let grayList = [];
 
@@ -126,17 +138,24 @@ const resolveDependencies = async({pkgName,version}) => {
 
 };
 
+// Downloads a single package at a time with a given name and version
 const downloadPkg = async ({pkgName,version}) => {
 
     // Pass the name of the package and the version and it will download the package from the npm-registry
     // Downloads a .tgz file
+
+    // Resolves the correct version
     let ver = await resolveVersion({pkgName,version});
+
+    // API call to the registry
     try{
         let response = await axios({
             method:"GET",
             url:baseRegistryUrl + `/${pkgName}/-/${pkgName}-${ver}.tgz`,
             responseType:'stream'
         });
+
+        // Pipe the contents to the tarball on disk. The uses targz to decompress and save it.
         let downloading = response.data.pipe(fs.createWriteStream(`${pkgName}-${ver}.tgz`));
         downloading.on('finish',  () => {
             targz.decompress({
@@ -150,6 +169,7 @@ const downloadPkg = async ({pkgName,version}) => {
                         if(err)
                             console.log(err);
                         else{
+                            // Removes the tarball and the extra package
                             rimraf.sync(`npkg_modules/${pkgName}-${ver}/`);
                             rimraf.sync(`${pkgName}-${ver}.tgz`)
                         }
@@ -166,7 +186,8 @@ const downloadPkg = async ({pkgName,version}) => {
 };
 
 
-
+// Helper function to run the program.
+// Will be replaced with a better function in production env
 async function display() {
 
     let args = process.argv.splice(2);
